12b18de7ba70bf1f789e1892c11fdbb0982d151a
SpringNyan <springnyan@gmail.com>
2026-08-21 14:39:49 +0000

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  endOfLine: "lf",
+  printWidth: 80,
+  tabWidth: 2,
+  trailingComma: "all",
+};
+
+export default config;
